3e403ab204689f07a10b7ff01a5a24d287a7ac11
mituru1145-source <mituru1145@gmail.com>
2026-04-22 00:48:59 +0900

    kadaiをプッシュ

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+const today = new Date()
+console.log(today);
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const date = today.getDate();
+
+console.log(year);
+console.log(month);
+console.log(date);
+console.log(year + '年' + month + '月' + date + '日');
